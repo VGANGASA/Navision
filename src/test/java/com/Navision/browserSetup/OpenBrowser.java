b7f6ba3acf5b3802a8139e5d57a0065fb03af2c7
Vijay <vijaya.gangasani@sogeti.com>
2018-03-22 10:35:00 +0000

    changes1

--- src/test/java/com/Navision/browserSetup/OpenBrowser.java
+++ src/test/java/com/Navision/browserSetup/OpenBrowser.java
@@ -21,55 +21,47 @@ import com.Navision.utility.Screenshots;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
-
-
 public class OpenBrowser {
-	
+
 	Screenshots ss = new Screenshots(driver);
 	protected static WebDriver driver;
-	
+
 	public String errorMessage = "no such element exception: ";
-	
+
 	public ExtentReports report;
 	public ExtentTest logger;
-	
-	
-	
-    /*public void capturescreenshot(ITestResult result){
-		if(result.getStatus() == ITestResult.FAILURE){		
-			String ss_path = ss.captureScreenshot(driver, result.getName());
-			String image = logger.addScreenCapture(ss_path);
-			System.out.println("value for logger is : " + logger);
-			logger.log(LogStatus.FAIL, "Title Verification",image);
-			
-		} else if (result.getStatus() == ITestResult.SKIP){
-			String ss_path = ss.captureScreenshot(driver, result.getName());
-			String image = logger.addScreenCapture(ss_path);
-			logger.log(LogStatus.SKIP, "Title Verification",image);
-			
-		}else if(result.getStatus() == ITestResult.SUCCESS){
-			String ss_path = ss.captureScreenshot(driver, result.getName());
-			String image = logger.addScreenCapture(ss_path);
-			logger.log(LogStatus.PASS, "Title Verification",image);
-		}
-		
-		report.endTest(logger);
-		report.flush();
-	}
-    
-    public void startTest(){
-		report = new ExtentReports("../Navision/target/extentreport.html");		
-		
-//		report = new ExtentReports(System.getProperty("user.dir")+ "/test-output/coreproduct.html",true); 
-		report.addSystemInfo("Host Name", "Navision DevOps")
-		.addSystemInfo("Environment","https://talent.capgemini.com/uk")
-		.addSystemInfo("User Name", "Vijay");
-		
-//		report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
-	} 
-	*/
-    
-   
+
+	/*
+	 * public void capturescreenshot(ITestResult result){ if(result.getStatus() ==
+	 * ITestResult.FAILURE){ String ss_path = ss.captureScreenshot(driver,
+	 * result.getName()); String image = logger.addScreenCapture(ss_path);
+	 * System.out.println("value for logger is : " + logger);
+	 * logger.log(LogStatus.FAIL, "Title Verification",image);
+	 * 
+	 * } else if (result.getStatus() == ITestResult.SKIP){ String ss_path =
+	 * ss.captureScreenshot(driver, result.getName()); String image =
+	 * logger.addScreenCapture(ss_path); logger.log(LogStatus.SKIP,
+	 * "Title Verification",image);
+	 * 
+	 * }else if(result.getStatus() == ITestResult.SUCCESS){ String ss_path =
+	 * ss.captureScreenshot(driver, result.getName()); String image =
+	 * logger.addScreenCapture(ss_path); logger.log(LogStatus.PASS,
+	 * "Title Verification",image); }
+	 * 
+	 * report.endTest(logger); report.flush(); }
+	 * 
+	 * public void startTest(){ report = new
+	 * ExtentReports("../Navision/target/extentreport.html");
+	 * 
+	 * // report = new ExtentReports(System.getProperty("user.dir")+
+	 * "/test-output/coreproduct.html",true); report.addSystemInfo("Host Name",
+	 * "Navision DevOps")
+	 * .addSystemInfo("Environment","https://talent.capgemini.com/uk")
+	 * .addSystemInfo("User Name", "Vijay");
+	 * 
+	 * // report.loadConfig(new File(System.getProperty("user.dir") +
+	 * "\\extent-config.xml")); }
+	 */
 
 	public static void setInstance(String browserName) {
 
@@ -92,12 +84,12 @@ public class OpenBrowser {
 	}
 
 	private static WebDriver setChromeDriver() {
-		
-		System.setProperty("webdriver.chrome.driver", "../Navision/lib/chromedriver.exe");
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/lib/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://talent.capgemini.com/uk");
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		return driver;
 	}
 
@@ -109,8 +101,5 @@ public class OpenBrowser {
 		driver.get("https://talent.capgemini.com/uk");
 		return driver;
 	}
-
-
-    
 
 }
